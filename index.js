7fec8513a6da4bb8f55dd19752697c2eb7457e88
Eirik Joneid <t0wn@pop-os.localdomain>
2020-03-15 17:41:00 +0100

    Channel name check

--- index.js
+++ index.js
@@ -1,6 +1,40 @@
-import {BOT_TOKEN} from 'config.js';
 
+const BOT_TOKEN = require('./config');
 const Discord = require('discord.js');
-const bot = Discord.Client();
+const bot = new Discord.Client();
 
 const token = BOT_TOKEN;
+
+bot.on('ready', () => {
+    console.log('Bot online');
+});
+
+
+
+var ashamed_users = [];
+
+async function getChannelName(channelID) {
+    const name = await bot.channels.fetch(channelID).then((channel) => (channel.name) );
+    
+    return name;
+}
+
+bot.on('voiceStateUpdate', (oldMember, newMember) => {
+    console.log('VoiceStateChange');
+    // console.log(oldMember);
+    // console.log(newMember);
+
+    (async () => {
+        var newChannelName = await getChannelName(newMember.channelID);
+    
+
+        if (newChannelName == 'Skammekroken') {
+            console.log('Skam deg!');
+            
+        }
+    })();
+    
+});
+
+
+bot.login(token);
