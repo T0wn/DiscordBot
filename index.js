9573d73971079b33f4c61c8752024cfb3dd57966
Eirik Joneid <t0wn@pop-os.localdomain>
2020-03-15 21:49:40 +0100

    eslint add

--- index.js
+++ index.js
@@ -1,21 +1,16 @@
-
-const BOT_TOKEN = require('./config');
+const { TOKEN } = require('./config.json');
 const Discord = require('discord.js');
 const bot = new Discord.Client();
-
-const token = BOT_TOKEN;
 
 bot.on('ready', () => {
     console.log('Bot online');
 });
 
 
-
-var ashamed_users = [];
+// let ashamed_users = [];
 
 async function getChannelName(channelID) {
-    const name = await bot.channels.fetch(channelID).then((channel) => (channel.name) );
-    
+    const name = await bot.channels.fetch(channelID).then((channel) => (channel.name));
     return name;
 }
 
@@ -25,16 +20,16 @@ bot.on('voiceStateUpdate', (oldMember, newMember) => {
     // console.log(newMember);
 
     (async () => {
-        var newChannelName = await getChannelName(newMember.channelID);
-    
+        const newChannelName = await getChannelName(newMember.channelID);
+        const user = await oldMember.guild.members.fetch('153951942677233665').then((member) => (member.user));
+
+        console.log(user);
 
         if (newChannelName == 'Skammekroken') {
             console.log('Skam deg!');
-            
         }
     })();
-    
 });
 
 
-bot.login(token);
+bot.login(TOKEN);
