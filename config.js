c7b75d3a9245f3bcb75261ad049fb7b5c3019a83
Eirik Joneid <t0wn@pop-os.localdomain>
2020-03-15 15:36:03 +0100

    project init

---
+++ config.js
@@ -0,0 +1,9 @@
+const config = {
+    bot_token = '',
+}
+
+const BOT_TOKEN = config.bot_token;
+
+export {BOT_TOKEN};
+
+export default config;
---
+++ index.js
@@ -0,0 +1,6 @@
+import {BOT_TOKEN} from 'config.js';
+
+const Discord = require('discord.js');
+const bot = Discord.Client();
+
+const token = BOT_TOKEN;
