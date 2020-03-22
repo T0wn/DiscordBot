c766b26b852dfe6e923f9738cc2ff9c318e68a1a
Eirik Joneid <t0wn@pop-os.localdomain>
2020-03-22 02:26:18 +0100

    plays audio on skammekroken-join

--- index.js
+++ index.js
@@ -10,10 +10,12 @@ bot.on('ready', () => {
 const ashamed_users = [];
 
 // voiceStateUpdate triggres på all voiceChannel change, inkludert mute, deafen, etc.
-bot.on('voiceStateUpdate', (oldMember, newMember) => {
+bot.on('voiceStateUpdate', async (oldMember, newMember) => {
     console.log('VoiceStateChange');
 
     const verdiloosRole = newMember.guild.roles.cache.find(role => role.name == 'Verdiløs');
+    // const skammekrokenChannel = newMember.guild.channels.cache.find(channel => channel.name == 'Skammekroken');
+    // console.log(skammekrokenChannel);
 
     const newChannel = newMember.channel;
     const oldChannel = oldMember.channel;
@@ -30,6 +32,14 @@ bot.on('voiceStateUpdate', (oldMember, newMember) => {
             ashamed_users.push({ userID: oldUser.id, roles: oldUser.roles.cache });
             newUser.roles.set([verdiloosRole]);
             newUser.voice.setMute(true);
+
+            const connenction = await newChannel.join();
+            const dispatcher = connenction.play('./audio/shame-1.mp3', { volume: 0.5 });
+
+            dispatcher.on('finish', () => {
+                dispatcher.destroy();
+                connenction.disconnect();
+            });
         }
         else if (newChannel != 'Skammekroken' && newChannel != null && oldChannel.id != newChannel.id) {
             const originalUser = ashamed_users.find(usr => usr.userID == oldUser.id);
