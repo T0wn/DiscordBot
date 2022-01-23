df009de83e61b01c01041c415eddfe584402b9a0
Eirik Joneid <eirijomine@hotmail.com>
2022-01-23 22:59:28 +0100

    cleanup

--- src/DictatorBot.py
+++ src/DictatorBot.py
@@ -4,10 +4,13 @@ import requests
 import io
 import asyncio
 from PIL import Image
+from discord.ext import commands
+
 
 class DictatorBot(discord.Client):
     def __init__(self, skammekroken="Skammekroken", hornyjail="Hornyjail", verdilos="Verdiløs"):
         super().__init__()
+        self.bot = commands.Bot(command_prefix='$')
 
         # variabelnavn in case man vil endre navnet på skammekroken kanalen eller verdiløs rollen. 
         self.skammekroken = skammekroken
@@ -17,85 +20,70 @@ class DictatorBot(discord.Client):
         # lagrer rollene til folk som er verdiløse, slik at de kan få de samme rollene tilbake igjen.
         self.shamedMembers = {}
 
-
-
     async def on_ready(self):
         print(f'Logged on as {self.user}')
-        
+
         version = os.getenv("VERSION")
         if version is not None:
-            activityVar = discord.Game(f"{version}")
-            await self.change_presence(status=discord.Status.online, activity=activityVar)
-
-
+            activity_var = discord.Game(f"{version}")
+            await self.change_presence(status=discord.Status.online, activity=activity_var)
 
     # Denne kjøres hver gang noe i voice kanalene endrer seg (mute, join, leave, etc..).
-    async def on_voice_state_update(self, member, oldVoiceState, newVoiceState):
-        action = self.get_action(member, oldVoiceState, newVoiceState)
+    async def on_voice_state_update(self, member, old_voice_state, new_voice_state):
+        action = self.get_action(member, old_voice_state, new_voice_state)
         if action == "shame":
             await self.shame(member)
         elif action == "jailshame":
-            await self.jailShame(member)
+            await self.jail_shame(member)
         elif action == "redeem":
             await self.redeem(member)
 
-
-
     # sjekker om en bruker skal shames.
-    def get_action(self, member, oldVoiceState, newVoiceState):
+    def get_action(self, member, old_voice_state, new_voice_state):
         if member != self.user:
-            if oldVoiceState.channel and newVoiceState.channel:
+            if old_voice_state.channel and new_voice_state.channel:
 
-                if newVoiceState != oldVoiceState:
-                    if newVoiceState.channel.name == self.skammekroken:
+                if new_voice_state != old_voice_state:
+                    if new_voice_state.channel.name == self.skammekroken:
                         return "shame"
-                    if newVoiceState.channel.name == self.hornyjail:
+                    if new_voice_state.channel.name == self.hornyjail:
                         return "jailshame"
 
-                if newVoiceState.channel:
-                    if newVoiceState.channel.name != self.skammekroken and oldVoiceState.channel.name == self.skammekroken:
+                if new_voice_state.channel:
+                    if new_voice_state.channel.name != self.skammekroken and old_voice_state.channel.name == self.skammekroken:
                         return "redeem"
-                    if newVoiceState.channel.name != self.hornyjail and oldVoiceState.channel.name == self.hornyjail:
+                    if new_voice_state.channel.name != self.hornyjail and old_voice_state.channel.name == self.hornyjail:
                         return "redeem"
-                    
+
         return "nothing"
 
-
-
     def add_shamed_member(self, member):
-        memberInfo = {'user': member, 'roles': member.roles}
+        member_info = {'user': member, 'roles': member.roles}
         uid = f"{member.id} {member.guild.id}"
-        self.shamedMembers[uid] = memberInfo
-
-
+        self.shamedMembers[uid] = member_info
 
     def pop_shamed_member(self, member):
         uid = f"{member.id} {member.guild.id}"
         roles_to_assign = self.shamedMembers.pop(uid)['roles'][1:]
         return roles_to_assign
-        
-
 
     # henter verdiløs rollen i serveren, hvis den finnes
-    def get_verdiloosRole(self, member):
+    def get_verdiloos_role(self, member):
         try:
             return next(role for role in member.guild.roles if role.name == self.verdilos)
         except StopIteration:
-            raise StopIteration("could not find verdiløs-role in guild")
+            raise StopIteration("could not find verdiloos-role in guild")
 
-
-    
     async def set_member_verdiloos(self, member):
         # henter verdiløs rollen i serveren, hvis den finnes
-        verdiloosRole = self.get_verdiloosRole(member)
-        
+        verdiloos_role = self.get_verdiloos_role(member)
+
         # Lagrer brukeren og rollene han hadde før alle rollene blir fjerna.
         # Gir brukeren verdiløs rollen.
         self.add_shamed_member(member)
-        await member.remove_roles(*member.roles[1:]) # bruker slicer til å fjerne everyone rollen fra listen. Den kan ikke fjernes fra brukere.
-        await member.add_roles(verdiloosRole)
-
-
+        # bruker slicer til å fjerne everyone rollen fra listen. Den kan ikke fjernes fra brukere.
+        await member.remove_roles(*member.roles[1:])
+        await member.add_roles(verdiloos_role)
 
     async def shame(self, member):
         await self.set_member_verdiloos(member)
@@ -104,42 +92,40 @@ class DictatorBot(discord.Client):
         try:
             connection = await member.voice.channel.connect()
             audio = discord.FFmpegPCMAudio("./audio/shame-1.mp3")
-            player = connection.play(audio)
+            connection.play(audio)
             while connection.is_playing() and len(member.voice.channel.voice_states) > 1:
                 await asyncio.sleep(1)
             await connection.disconnect()
-        except discord.errors.ClientException: # Denne kastes hvis boten allerede er connecta til voicechannelen, f.eks hvis vi kaster 2 personer i skammekroken etter hverandre.
+        except discord.errors.ClientException:
+            # Denne kastes hvis boten allerede er connecta til voicechannelen,
+            # f.eks hvis vi kaster 2 personer i skammekroken etter hverandre.
             return
         except Exception as e:
             print(e)
 
-
-
-    async def jailShame(self, member):
+    async def jail_shame(self, member):
         await self.set_member_verdiloos(member)
 
         avatar = Image.open(requests.get(member.avatar_url, stream=True).raw)
         avatar = avatar.resize((400, 400))
 
-        memeImg = Image.open("images/meme.png")
-        memeImg.paste(avatar, (900, 450))
-        
-        memeImgByteArr = io.BytesIO()
-        memeImg.save(memeImgByteArr, format='PNG')
-        memeImgByteArr.seek(0)
-        imgSend = discord.File(memeImgByteArr, filename="hornyjailmeme.png")
+        meme_img = Image.open("images/meme.png")
+        meme_img.paste(avatar, (900, 450))
 
-        await member.guild.system_channel.send(f"{member.mention}", file=imgSend)
-        
+        meme_img_byte_arr = io.BytesIO()
+        meme_img.save(meme_img_byte_arr, format='PNG')
+        meme_img_byte_arr.seek(0)
+        img_send = discord.File(meme_img_byte_arr, filename="hornyjailmeme.png")
 
-        
+        await member.guild.system_channel.send(f"{member.mention}", file=img_send)
+
     async def redeem(self, member):
         # fjerner bruker infoen fra shamed users, og henter rollene som skal gies tilbake
         try:
             roles_to_assign = self.pop_shamed_member(member)
         except KeyError as e:
-            print("Error: Could not find member in shamedmembers")
+            print("Error: Could not find member among shamed members")
             return
-        verdiloosRole = self.get_verdiloosRole(member)
+        verdiloos_role = self.get_verdiloos_role(member)
         await member.add_roles(*roles_to_assign)
-        await member.remove_roles(verdiloosRole)
+        await member.remove_roles(verdiloos_role)
